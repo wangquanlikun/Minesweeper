91ffe5532f505b87c57f2c9376cc43b3b6e1af56
Wang Quan <75524809+wangquanlikun@users.noreply.github.com>
2023-04-27 23:49:38 +0800

    一个demo版本的实现自定义大小游戏区域生成、识别点击、在给定的方块打开或标记操作的CPP文件。基于EasyX
    头文件：一个样例自定义的头文件。在这里声明使用的函数原型，定义符号常量等

---
+++ 图形化版本/head.h
@@ -0,0 +1,16 @@
+#ifndef GUI_HEAD
+#define GUI_HEAD
+
+#define WIDTH 1280
+#define HEIGHT 720
+
+void opening();
+void gamedraw();
+void remainmine();
+void openspace(int k1, int k2);
+void markmine(int k1, int k2);
+void markquestion(int k1, int k2);
+
+void printnum(int x, int y, int printnum);
+
+#endif
