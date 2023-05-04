9043b2bcf67868f59ca2078c6d532fa622ef742d
Wang Quan <75524809+wangquanlikun@users.noreply.github.com>
2023-05-04 09:24:46 +0800

    Update head.h
    增加了一些函数的声明

--- 图形化版本/head.h
+++ 图形化版本/head.h
@@ -4,12 +4,20 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
+#define BACKGROUNDCOLOR 0xE7EFDE
+
+#include<Windows.h>
+
 void opening();
 void gamedraw();
-void remainmine();
+void remainmine(int remain);
+
+void operation(int operatype, int k1, int k2);
+
 void openspace(int k1, int k2);
 void markmine(int k1, int k2);
 void markquestion(int k1, int k2);
+void unopenspace(int k1, int k2);
 
 void printnum(int x, int y, int printnum);
 
