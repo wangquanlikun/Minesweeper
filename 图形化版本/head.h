06da0db47258eaa876145c370b8c71b0f87ae643
Wang Quan <75524809+wangquanlikun@users.noreply.github.com>
2023-05-04 21:24:51 +0800

    Update head.h
    增加了一些函数的声明，常量的定义

--- 图形化版本/head.h
+++ 图形化版本/head.h
@@ -12,12 +12,21 @@ void opening();
 
 void creatspace(int k1,int k2);//k1,k2是第一次点击的位置。不能有雷
 void gamedraw();
+
 void remainmine(int remain);
+
+void gamestatusprint(int status);
+#define GAMEINPROGRESS 1
+#define HITMINE 2
+#define GAMEWIN 3
 
 int operation(int operatype, int xspace, int yspace);
 #define LEFTCLICK 1
 #define RIGHTCLICK 2
 #define BOTHCLICK 3
+
+bool adjustwin();
+void gameover(int status);
 
 void openspace(int k1, int k2);
 void markmine(int k1, int k2);
